78d0adccd5b78feed6e2b220236fbabeae22b5af
pawan <pawanshinde87@gmail.com>
2018-02-12 13:44:17 +0530

    Removed old files

--- Shell-Variables,Grab-User-Input.sh
+++
@@ -1,4 +0,0 @@
-#This script demonstrate the uses of read command.
-echo "Please enter your name:"
-read ip_name
-echo "Hello $ip_name, It's a fine day, isn't it ?"
--- ssh1.sh
+++
@@ -1,4 +0,0 @@
-echo "This is some common operations we have"
-ls -l
-pwd
-banner "Hello World"
